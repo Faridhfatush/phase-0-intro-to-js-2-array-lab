29973546903980481e4c0f13e44018ec9432c0c6
Fatuma Dahir <faridhfatush2024@gmail.com>
2025-04-23 13:57:55 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat (){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function  destructivelyRemoveLastCat (){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat (){ 
+    cats.shift()
+}
+
+function appendCat(name){
+    const newCats = [...cats, name]
+    return newCats
+}
+
+const newCats = appendCat("Broom")
+
+function prependCat(name){
+    const newsecCats = [name, ...cats]
+    return newsecCats;
+}
+const newsecCats = prependCat("Arnold")
+
+function removeLastCat(){
+   
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
